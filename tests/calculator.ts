2d5ea6b06e011bce766739cae1a2ecd1c4533004
Brent-Honeycutt <131912805+Brent-Honeycutt@users.noreply.github.com>
2023-05-08 21:13:03 -0400

    commented invalid tests

--- tests/calculator.ts
+++ tests/calculator.ts
@@ -1,22 +1,22 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  page.on('console', msg => console.log(msg.text()))
-});
+// test.beforeEach(async ({ page }) => {
+//   await page.goto('http://localhost:3000/');
+//   page.on('console', msg => console.log(msg.text()))
+// });
 
-test('has title', async ({ page }) => {
+// test('has title', async ({ page }) => {
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
+//   // Expects the URL to contain intro.
+//   await expect(page).toHaveURL(/.*intro/);
+// });
